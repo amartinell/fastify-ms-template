4c143f015167c333db0393af46cee456c162b36a
Andrea Martinelli <andrea.martinelli@translated.net>
2024-03-30 14:05:50 +0100

    Adding node async local storage as fastify plugin

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
 import fastify from 'fastify'
 import routes from "./routes/router";
+import {fastifyRequestContext} from "@fastify/request-context";
+import {nanoid} from "nanoid";
 
 const main = async () => {
   const app = fastify();
+
+  // Import type schema validator
+  app.register(import('@fastify/sensible'));
+
+  // Register request context plugin
+  // This works like a middleware that runs before each request
+  // adding local storage to the request object
+  app.register(fastifyRequestContext, {
+    hook: 'onRequest',
+    defaultStoreValues: {
+      info: {
+        idTransaction: nanoid(10),
+      },
+    }
+  });
+
+  // Register routes
   app.register(routes);
 
   try {
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,13 +2,9 @@ import healthcheck from "../controllers/healthcheck";
 import {FastifyPluginAsyncTypebox} from "@fastify/type-provider-typebox";
 import {HealthcheckRouteSchema} from "./schemes/healthcheck";
 
-const buildServer: FastifyPluginAsyncTypebox = async (app): Promise<void> => {
-  app.register(import('@fastify/sensible'));
-
-  // Registering middlewares
-
+const router: FastifyPluginAsyncTypebox = async (app): Promise<void> => {
   // Routes
   app.get('/healthcheck', HealthcheckRouteSchema, healthcheck(app));
 }
 
-export default buildServer;
+export default router;
